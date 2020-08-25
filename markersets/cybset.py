d958edbb42160870736c4d03c6040bab8c8b269e
Bálint Hodossy <bkh16@ic.ac.uk>
2020-08-25 23:03:02 +0100

    First version of CybSet segment functions, and virtual marker method

--- markersets/cybset.py
+++ markersets/cybset.py
@@ -1,6 +1,7 @@
 from jcs import *
 from functools import partial as p
 
+
 class CybSet(MarkerSet):
 
     def __init__(self, c3d_file, emg_file=None):
@@ -17,21 +18,55 @@ class CybSet(MarkerSet):
                             'RTOE', 'LTOE']
 
         self.dict_segment = {'Pelvis': self.pelvis_seg,
-                             'RThigh': p(self.thigh_seg, s='R'),    'LThigh': p(self.thigh_seg, s='L'),
-                             'RShank': p(self.shank_seg, s='R'),    'LShank': p(self.shank_seg, s='L'),
-                             'RFoot': p(self.foot_seg, s='R'),      'LFoot': p(self.foot_seg, s='L')}
+                             'RThigh': p(self.thigh_seg, s='R'), 'LThigh': p(self.thigh_seg, s='L'),
+                             'RShank': p(self.shank_seg, s='R'), 'LShank': p(self.shank_seg, s='L'),
+                             'RFoot': p(self.foot_seg, s='R'), 'LFoot': p(self.foot_seg, s='L')}
 
-        self.dict_joint = {'RHip': ('Pelvis', 'RThigh', 'R'),   'LHip': ('Pelvis', 'LThigh', 'L'),
-                           'RKnee': ('RThigh', 'RShank', 'R'),  'LKnee': ('LThigh', 'LShank', 'L'),
-                           'RFoot': ('RShank', 'RFoot', 'R'),   'LFoot': ('LShank', 'LFoot', 'L')}
+        self.dict_joint = {'RHip': ('Pelvis', 'RThigh', 'R'), 'LHip': ('Pelvis', 'LThigh', 'L'),
+                           'RKnee': ('RThigh', 'RShank', 'R'), 'LKnee': ('LThigh', 'LShank', 'L'),
+                           'RFoot': ('RShank', 'RFoot', 'R'), 'LFoot': ('LShank', 'LFoot', 'L')}
+
+    def marker_preproc(self):
+        # Define virtual markers
+        self.dict_marker['LVAN'] = (self.dict_marker['LANK'] + self.dict_marker['LANM']) / 2
+        self.dict_marker['RVAN'] = (self.dict_marker['RANK'] + self.dict_marker['RANM']) / 2
+        self.dict_marker['LVKN'] = (self.dict_marker['LKNE'] + self.dict_marker['LKNM']) / 2
+        self.dict_marker['RVKN'] = (self.dict_marker['RKNE'] + self.dict_marker['RKNM']) / 2
+
+        # Hip joint center based on Vaughan, Davis and Connor: Dynamics of Human Gait
+        v_hip = self.dict_marker['LASI'] - self.dict_marker['RASI']
+        v_norm = np.linalg.norm(v_hip, axis=1)
+        asis_breadth = np.mean(v_norm)
+        v_hip = v_hip / v_norm[:, None]  # Broadcasting division for each row
+
+        temp_hip = self.dict_marker['LASI'] - self.dict_marker['SACR']
+        w_hip = norm(np.cross(temp_hip, v_hip))
+        u_hip = np.cross(v_hip, w_hip)
+        self.dict_marker['LVHI'] = self.dict_marker['SACR'] + asis_breadth * (
+                0.598 * u_hip + 0.344 * v_hip - 0.29 * w_hip)
+        self.dict_marker['RVHI'] = self.dict_marker['SACR'] + asis_breadth * (
+                0.598 * u_hip - 0.344 * v_hip - 0.29 * w_hip)
 
     def pelvis_seg(self):
-        return
+        i_vect = (self.dict_marker['RASI'] - self.dict_marker['LASI'])
+        k_vect = np.cross(self.dict_marker['RASI'] - self.dict_marker['SACR'],
+                          self.dict_marker['LASI'] - self.dict_marker['SACR'], axis=1)
+        j_vect = np.cross(k_vect, i_vect)
+        return Segment(lateral=i_vect, frontal=j_vect, longitudinal=k_vect, name='Pelvis')
 
     def thigh_seg(self, s='R'):
-        return
+        k_vect = self.dict_marker[s + 'VHI'] - self.dict_marker[s + 'VKN']
+        j_vect = (-1 if s == 'L' else 1) * \
+                 np.cross(k_vect, self.dict_marker[s + 'THI'] - self.dict_marker[s + 'VHI'], axis=1)
+        i_vect = np.cross(j_vect, k_vect)
+        return Segment(lateral=i_vect, frontal=j_vect, longitudinal=k_vect, name=s + 'Thigh')
 
     def shank_seg(self, s='R'):
+        k_vect = self.dict_marker[s + 'VKN'] - self.dict_marker[s + 'VAN']
+        j_vect = (-1 if s == 'L' else 1) * \
+                 np.cross(k_vect, self.dict_marker[s + 'TIB'] - self.dict_marker[s + 'VKN'], axis=1)
+        i_vect = np.cross(j_vect, k_vect)
+        return Segment(lateral=i_vect, frontal=j_vect, longitudinal=k_vect, name=s + 'Shank')
         return
 
     def foot_seg(self, s='R'):
